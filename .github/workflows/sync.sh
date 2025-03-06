1164c6046813aac45e05820e5ee7fdd7f63ebb4f
trindadedev13 <trindadedev13@gmail.com>
2025-03-06 11:26:03 -0300

    feat: auto mirror sync

---
+++ .github/workflows/sync.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+UPSTREAM="upstream"
+ORIGIN="origin"
+BRANCH="main"
+
+if [ "$1" == "github" ]; then
+  echo "Configuring..."
+  git remote add $UPSTREAM https://android.googlesource.com/platform/packages/apps/Launcher3
+fi
+
+echo "Fetching $UPSTREAM..."
+git fetch $UPSTREAM
+
+echo "Merging $UPSTREAM in $BRANCH..."
+git merge $UPSTREAM/$BRANCH
+
+echo "Making push in $ORIGIN.."
+git push $ORIGIN $BRANCH
