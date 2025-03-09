ff2474cb4d4e152e96613d4d5ae53802d6e5bb22
Aquiles Trindade <trindadedev13@gmail.com>
2025-03-08 23:50:32 -0300

    fix: error when push changes

--- sync.sh
+++ sync.sh
@@ -12,9 +12,18 @@ fi
 echo "Fetching $UPSTREAM..."
 git fetch $UPSTREAM
 
+# First, checkout the branch
+echo "Checking out branch $BRANCH..."
+git checkout $BRANCH
+
+# If there are local changes to sync.sh, restore it
+echo "Restoring sync.sh to the state of the branch..."
+git checkout -- sync.sh
+
+# Now reset the local branch to align with the upstream branch
 echo "Resetting $BRANCH to $UPSTREAM/$BRANCH..."
-git checkout $BRANCH sync.sh
 git reset --hard $UPSTREAM/$BRANCH
 
-echo "Making push in $ORIGIN..."
+# Push the changes to the origin repository
+echo "Pushing changes to $ORIGIN..."
 git push $ORIGIN $BRANCH --force
